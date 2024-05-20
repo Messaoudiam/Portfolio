168d6d530cdbcc57e2829b9a7021a661f7c026b1
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-05-20 13:32:05 +0200

    theme added

--- app/_landing/Hero.tsx
+++ app/_landing/Hero.tsx
@@ -3,7 +3,8 @@ import { Section } from "./Section"
 export const Hero = () => {
   return (
    <Section>
-    <h1 className="text-4xl font-bold"> Hey I am Messaoud </h1>
+    <h1 className="text-4xl font-bold"> Hey, I am Messaoud </h1>
+    <p> Web developer </p>
    </Section>
   )
 }
